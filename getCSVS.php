b6955336ff371549b9712d43fc5999b4f9fe1cd9
dev1 <dev1@bzs.com.br>
2021-04-29 08:58:13 -0300

    Fim

--- getCSVS.php
+++ getCSVS.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-    //minhoca
-=======
-    //teste
->>>>>>> bfa06813d25de4aab0286204204df74104a94f5c
+
     require_once 'localconection.php';
     require_once 'classes/Agente.php';
     require_once 'classes/Empresa.php';
